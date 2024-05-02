905e2d54a0a3d0e93edad6914e2f7557fecd41ac
janna <jannakondeth5@gmail.com>
2024-05-02 13:38:38 +0530

    jwt and bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 
 const userSchema=new Schema({
@@ -21,6 +23,33 @@ const userSchema=new Schema({
         default:false
     }
 },{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password")) return next()
+
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateToken=async function(){
+   return jwt.sign({
+         _id:this._id,
+         name:this.name,
+         email:this.email,
+         password:this.password
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+       expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+
 
 
 export const User=mongoose.model("User",userSchema)
